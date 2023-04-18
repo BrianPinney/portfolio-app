dcdb178cd588e662edd6a6498d41fba97ed0deea
Brian J. Pinney <brianj.pinney@gmail.com>
2023-04-18 08:50:30 -0400

    Made spelling corrections

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,14 +4,12 @@ export default function AboutMe(){
 
     return (
     <>
-        <div> 
-            <h1>Brian J. Pinney</h1>
-            <p>I am a person of many hobbies and interests. Craft beer Brewing, Bushcrafting, Bladesmithing, WhiteWater Kayaking,
-               Camping, Gaming, biking (motor, mountain, and road). I am also a tech enthusiest. All my hobbies envolve a certain level of 
+        <h1>Brian J. Pinney</h1>
+        <p>I am a person of many hobbies and interests. Craft beer Brewing, Bushcrafting, Bladesmithing, WhiteWater Kayaking,
+               Camping, Gaming, biking (motor, mountain, and road). I am also a tech enthusiast. All my hobbies involve a certain level of 
                technology and as time goes buy I find myself wanting and learning more about different technologies that revolve around all
                the things I love to do.
-            </p>
-        </div>
+        </p> 
     </>
     )
 }
