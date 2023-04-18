3dc3754ed0c858be57292fd403131ffddd3b5389
Brian J. Pinney <brianj.pinney@gmail.com>
2023-04-18 19:10:43 -0400

    Added photo to container.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,12 +4,14 @@ export default function AboutMe(){
 
     return (
     <>
-        <h1>Brian J. Pinney</h1>
-        <p>I am a person of many hobbies and interests. Craft beer Brewing, Bushcrafting, Bladesmithing, WhiteWater Kayaking,
-               Camping, Gaming, biking (motor, mountain, and road). I am also a tech enthusiast. All my hobbies involve a certain level of 
-               technology and as time goes buy I find myself wanting and learning more about different technologies that revolve around all
-               the things I love to do.
-        </p> 
+    <section className="container" m>
+       <h1>Brian J. Pinney</h1>
+    </section>
+    <p>I am a person of many hobbies and interests. Craft beer Brewing, Bushcrafting, Bladesmithing, WhiteWater Kayaking,
+                Camping, Gaming, biking (motor, mountain, and road). I am also a tech enthusiast. All my hobbies involve a certain level of 
+                technology and as time goes buy I find myself wanting and learning more about different technologies that revolve around all
+                the things I love to do.
+    </p> 
     </>
     )
 }
