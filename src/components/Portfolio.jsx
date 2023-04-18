07c39330a483701f78555eab5f5deffcb8d681d8
Brian J. Pinney <brianj.pinney@gmail.com>
2023-04-17 23:42:52 -0400

    First commit started layout and rough draft

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function AboutMe(){
+
+    return (
+    <>
+        <div> 
+            <h1>Brian J. Pinney</h1>
+            <p>I am a person of many hobbies and interests. Craft beer Brewing, Bushcrafting, Bladesmithing, WhiteWater Kayaking,
+               Camping, Gaming, biking (motor, mountain, and road). I am also a tech enthusiest. All my hobbies envolve a certain level of 
+               technology and as time goes buy I find myself wanting and learning more about different technologies that revolve around all
+               the things I love to do.
+            </p>
+        </div>
+    </>
+    )
+}
